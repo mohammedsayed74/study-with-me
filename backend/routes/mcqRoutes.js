268c2552cb40e8b165be8c6910cc980706c4f9ba
Mohamed <mohamedsay48@gmail.com>
2026-04-17 09:08:41 +0200

    added MCQ schema and it's CRUD ops

---
+++ backend/routes/mcqRoutes.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const router = express.Router();
+
+const requireAuth = require('../middleware/requireAuth');
+
+const {
+    createQuestion,
+    getQuizQuestions,
+    verifyAnswer,
+    updateQuestion,
+    deleteQuestion
+} = require('../controllers/mcqControllers');
+
+router.use(requireAuth);
+
+router.post('/', createQuestion);
+router.get('/', getQuizQuestions);
+router.post('/verify', verifyAnswer);
+router.put('/:id', updateQuestion);
+router.delete('/:id', deleteQuestion);
+
+module.exports = router;
--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ const express = require(`express`);
 const userRoutes = require(`../backend/routes/userRoutes`);
 const courseRoutes = require(`../backend/routes/courseRoutes`);
 const materialRoutes = require("./routes/materialRoutes");
+const mcqRoutes = require('./routes/mcqRoutes');
 const mongoose = require("mongoose");
 const app = express();
 
@@ -20,6 +21,7 @@ mongoose
 app.use(`/api/users`, userRoutes);
 app.use(`/api/courses`, courseRoutes);
 app.use(`/api/materials`, materialRoutes);
+app.use(`/api/MCQs`, mcqRoutes);
 
 app.listen(process.env.PORT, () => {
   console.log(`server is running on port ${process.env.PORT}`);
